2abc9ab6d4b4ccb7c82f1488c379ec80c7b8569c
Daniel Maton Walker <danielw288@icloud.com>
2021-06-10 00:08:32 +0100

    ran into javascript issues
    cannot get error to show up

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let userName=document.getElementById("txtUser")
+let userName=document.getElementById("txtUserName")
 let email=document.getElementById("txtEmail")
 let pwd=document.getElementById("txtPwd")
 let conPwd=document.getElementById("txtCpwd")
 let form=document.querySelector("form")
 
 function validateinput(){
-    console.log("validate input")
+    //check username has been entered
+    if(userName.Value.trim()==""){
+        let parent=userName.parentElement;
+        let messageEle=parent.querySelector("small");
+        messageEle.style.visibility="Visible";
+        messageEle.innerText="User Name cannot be left blank";
+    }
+    else{ //should then hide error again
+        let parent=userName.parentElement;
+        let messageEle=parent.querySelector("small");
+        messageEle.style.visibility="Visible";
+        messageEle.innerText="";
+    }
 }
 
 document.querySelector("button")
 .addEventListener("click",(event)=>{
-    event.preventDefault()
-    validateinput()
+    event.preventDefault();
+    validateinput();
 });
